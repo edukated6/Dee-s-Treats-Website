4dc9134890be4cc8f8fc116aee921ed34d74bc72
edukated6 <edukateyourself@proton.me>
2026-03-22 21:20:30 -0500

    vercel v5

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 const app = express()
 
+// Enable CORS for GitHub Pages
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://edukated6.github.io')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200)
+  } else {
+    next()
+  }
+})
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
